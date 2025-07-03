cc45d3d1d20f8458c7613a057196a13ea41d70da
esjeruk25 <nadhifhakim259@gmail.com>
2025-07-03 11:23:03 +0700

    Initial commit

---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  //TODO: Implement HomeController
+
+  final string = ''.obs;
+  final integer = 0.obs;
+  final float = 0.0.obs;
+  final boolean = false.obs;
+
+}
